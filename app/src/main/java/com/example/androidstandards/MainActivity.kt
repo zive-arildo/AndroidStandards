8df708ae46c446b3d70589140ae1a0f7cc978f92
Arildo Junior <arildo.junior@ziveit.com>
2023-09-11 15:14:22 -0300

    Optimize imports

--- app/src/main/java/com/example/androidstandards/MainActivity.kt
+++ app/src/main/java/com/example/androidstandards/MainActivity.kt
@@ -10,14 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.androidstandards.ui.AndroidStandardsNavGraph
-import com.example.androidstandards.ui.screens.DetailScreen
-import com.example.androidstandards.ui.screens.HomeScreen
 import com.example.androidstandards.ui.theme.AndroidStandardsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/example/androidstandards/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/androidstandards/ui/screens/DetailScreen.kt
@@ -3,7 +3,6 @@ package com.example.androidstandards.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
